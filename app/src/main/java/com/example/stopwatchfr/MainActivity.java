7b2837eb9fa87b28d348c04b6273d43c8a064fc5
Rashad Wardhana <rashadwardhana@Rashads-MacBook-Pro.local>
2019-05-20 17:01:07 +0700

    Initial commit

---
+++ app/src/main/java/com/example/stopwatchfr/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.stopwatchfr;
+
+import android.os.SystemClock;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Chronometer;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Chronometer chronometer;
+    private boolean running;
+    private long pauseOffset;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        chronometer = findViewById(R.id.chronometer);
+
+    }
+
+    public void startChronometer(View view){
+        if(!running){
+            chronometer.setBase(SystemClock.elapsedRealtime() - pauseOffset);
+            chronometer.start();
+            running = true;
+        }
+    }
+
+    public void pauseChronometer(View view){
+        if(running){
+            chronometer.stop();
+            pauseOffset = SystemClock.elapsedRealtime() - chronometer.getBase();
+            running = false;
+        }
+    }
+
+    public void resetChronometer(View view){
+        chronometer.setBase(SystemClock.elapsedRealtime());
+        pauseOffset = 0;
+    }
+}
